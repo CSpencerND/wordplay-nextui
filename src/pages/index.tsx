53d6d8bf38dc2a56417339d9e87ea5b95bd1e6eb
cspencer <cspencernd@gmail.com>
2023-02-13 21:00:54 -0500

    size again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -229,14 +229,14 @@ const AboutBGFX = () => {
                 }}
                 css={{
                     objectFit: "fill",
-                    height: "200px",
+                    height: "100px",
                     "@xs": {
                         objectFit: "contain",
-                        maxHeight: "200px",
+                        maxHeight: "100px",
                     },
                     "@md": {
-                        height: "300px",
-                        maxHeight: "300px",
+                        height: "200px",
+                        maxHeight: "200px",
                     },
                 }}
             />
